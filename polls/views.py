5223d72b5fcc506a6773411d2875e4d31a0444f3
Erwin Alvarez <ealvarez@dcc.uchile.cl>
2012-12-29 16:39:54 -0300

    Django tutorial part 4

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render_to_response, get_object_or_404
-from django.http import HttpResponse
-from polls.models import Poll
-
-
-def index(request):
-    latest_polls = Poll.objects.all().order_by('-pub_date')[:5]
-    return render_to_response('polls/index.djhtml', {'latest_polls' : latest_polls})
-
-def details(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/details.djhtml', {'poll':poll})
-    
-def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from polls.models import Poll, Choice
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+    
+    poll = get_object_or_404(Poll, pk=poll_id)
+    choice = request.POST['choice']
+    try:
+        selected_choice = poll.choice_set.get(pk=choice)
+    except (KeyError, Choice.DoesNotExist):
+        return render_to_response('polls/details.html', {
+            'poll': poll,
+            'error_message': "You didn't select a choice.",
+        }, context_instance=RequestContext(request))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('poll_results', args=(poll.id,)))
